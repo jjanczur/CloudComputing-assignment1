24d785469f87a9896dd240ac04e7f0e7d4de96fd
Michal Zwolak <michal.zwolak@hotmail.com>
2018-11-14 21:25:33 +0100

    Initial commit and linpack modificiations

---
+++ linpack.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+EXECUTABLE="linpack"
+if [ ! -e $EXECUTABLE ] ; then
+	echo "Compiling linpack.c (requires GNU compiler collection)" 1>$2
+	gcc -O -o linpack linpack.c -lm
+fi
+
+echo "Running linpack benchmark" 1>&2
+
+if [ "$SYSTEMROOT" = "C:\Windows" ] ; then
+	result=$(./linpack.exe | tail -1)
+else
+	result=$(./${EXECUTABLE} | tail -1)
+fi
+echo "$result"
