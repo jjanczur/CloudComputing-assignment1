22046b8a98ea856193e3fc077cf9539ef2d4eb81
Jacek Janczura <janczura.jacek@gmail.com>
2018-11-19 16:39:28 +0100

    changed output to show local timestamp with memory benchmark measurement in sec

---
+++ memsweep.c
@@ -0,0 +1,42 @@
+/*
+**
+** memsweep.c        Benchmark to measure memory access performance
+**                   (measures time to access large heap array at
+**                   varying locations)
+**
+** To compile:  gcc -O -o memsweep memsweep.c -lm
+**
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <float.h>
+
+#define ARR_SIZE (8096 * 4096)
+
+static double second(void)
+{
+    return ((double)((double)clock()/(double)CLOCKS_PER_SEC));
+}
+
+void main(void)
+{
+  time_t now = time(0);
+  unsigned int i;
+  double time;
+  volatile char* array;
+  
+  array = (char*) malloc(sizeof(char)*ARR_SIZE);
+  
+  time = second();
+  for (i = 0; i < 10 * ARR_SIZE; i++)
+  {
+    array[(4096 * i + i) % ARR_SIZE] = 1 + array[i % ARR_SIZE];
+  }
+  free((void*)array);
+  time = second() - time;
+
+  printf(" %d, %.3f\n", now, time);
+}
